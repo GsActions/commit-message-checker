97edbdbf686c5909d5af4aaa98984f45fc381140
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2019-12-10 19:21:14 +0100

    [TASK] Add fix description

--- src/@types/graphql.d.ts
+++ src/@types/graphql.d.ts
@@ -1,3 +1,11 @@
+/** Temporary Fix
+ * 
+ * Fixes an issue of the GH actions toolkit see
+ * - https://github.com/actions/toolkit/issues/199
+ * - https://github.com/actions/toolkit/pull/228
+ * 
+ * This file should be removed once this change is in the official release.
+ */
 declare module '@octokit/graphql' {
   export type Variables = any
   export type GraphQlQueryResponse = any
