cf1afe2fa3549f48e23f25404cb1caaa686be5c6
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2020-09-20 23:49:35 +0200

    [TASK] Replace node-fetch by @octokit/graphql (#21)
    This patch replaces the node-fetch module by using the provided functionality
    of @octokit/graphql the official GitHub API instead.
    
    The tests are refactored and enhanced a little bit.
    
    Also various dependencies are updated and therefor the not longer needed
    graphql.d.ts is removed.

--- src/input-helper.ts
+++ src/input-helper.ts
@@ -20,8 +20,8 @@
  */
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import {graphql} from '@octokit/graphql'
 import {ICheckerArguments} from './commit-message-checker'
-const fetch = require('node-fetch')
 
 export interface PullRequestOptions {
   ignoreTitle: boolean
@@ -38,20 +38,33 @@ export interface PullRequestOptions {
 export async function getInputs(): Promise<ICheckerArguments> {
   const result = ({} as unknown) as ICheckerArguments
 
+  core.debug('Get inputs...')
+
   // Get pattern
   result.pattern = core.getInput('pattern', {required: true})
+  core.debug(`pattern: ${result.pattern}`)
 
   // Get flags
   result.flags = core.getInput('flags')
+  core.debug(`flags: ${result.flags}`)
 
   // Get error message
   result.error = core.getInput('error', {required: true})
+  core.debug(`error: ${result.error}`)
 
-  let excludeTitleStr = core.getInput('excludeTitle')
-  let excludeDescriptionStr = core.getInput('excludeDescription')
-  let checkAllCommitMessagesStr = core.getInput('checkAllCommitMessages')
+  // Get excludeTitle
+  const excludeTitleStr = core.getInput('excludeTitle')
+  core.debug(`excludeTitle: ${excludeTitleStr}`)
 
   // Get excludeDescription
+  const excludeDescriptionStr = core.getInput('excludeDescription')
+  core.debug(`excludeDescription: ${excludeDescriptionStr}`)
+
+  // Get checkAllCommitMessages
+  const checkAllCommitMessagesStr = core.getInput('checkAllCommitMessages')
+  core.debug(`checkAllCommitMessages: ${checkAllCommitMessagesStr}`)
+
+  // Set pullRequestOptions
   const pullRequestOptions: PullRequestOptions = {
     ignoreTitle: excludeTitleStr
       ? excludeTitleStr === 'true'
@@ -64,7 +77,9 @@ export async function getInputs(): Promise<ICheckerArguments> {
       : /* default */ false,
     accessToken: core.getInput('accessToken')
   }
-  // Get error message
+  core.debug(`accessToken: ${pullRequestOptions.accessToken}`)
+
+  // Get commit messages
   result.messages = await getMessages(pullRequestOptions)
 
   return result
@@ -79,53 +94,92 @@ export async function getInputs(): Promise<ICheckerArguments> {
 async function getMessages(
   pullRequestOptions: PullRequestOptions
 ): Promise<string[]> {
+  core.debug('Get messages...')
+  core.debug(
+    ` - pullRequestOptions: ${JSON.stringify(pullRequestOptions, null, 2)}`
+  )
+
   const messages: string[] = []
+
+  core.debug(` - eventName: ${github.context.eventName}`)
 
   switch (github.context.eventName) {
     case 'pull_request': {
-      if (
-        github.context.payload &&
-        github.context.payload.pull_request &&
-        github.context.payload.pull_request.title
-      ) {
-        let message: string = ''
+      if (!github.context.payload) {
+        throw new Error('No payload found in the context.')
+      }
 
-        if (!pullRequestOptions.ignoreTitle) {
-          message += github.context.payload.pull_request.title
+      if (!github.context.payload.pull_request) {
+        throw new Error('No pull_request found in the payload.')
+      }
+
+      let message = ''
+
+      // Handle pull request title and body
+      if (!pullRequestOptions.ignoreTitle) {
+        if (!github.context.payload.pull_request.title) {
+          throw new Error('No title found in the pull_request.')
         }
 
-        if (
-          github.context.payload.pull_request.body &&
-          !pullRequestOptions.ignoreDescription
-        ) {
+        message += github.context.payload.pull_request.title
+      } else {
+        core.debug(' - skipping title')
+      }
+
+      if (!pullRequestOptions.ignoreDescription) {
+        if (github.context.payload.pull_request.body) {
           message = message.concat(
             message !== '' ? '\n\n' : '',
             github.context.payload.pull_request.body
           )
         }
-        // dont add message if title and body were ignored
-        if (
-          !pullRequestOptions.ignoreTitle ||
-          !pullRequestOptions.ignoreDescription
-        ) {
-          messages.push(message)
-        }
       } else {
-        throw new Error(`No pull_request found in the payload.`)
+        core.debug(' - skipping description')
       }
 
+      if (message) {
+        messages.push(message)
+      }
+
+      // Handle pull request commits
       if (pullRequestOptions.checkAllCommitMessages) {
         if (!pullRequestOptions.accessToken) {
           throw new Error(
-            `checkAllCommitMessaages option requires a github access token.`
+            'The `checkAllCommitMessaages` option requires a github access token.'
           )
         }
+
+        if (!github.context.payload.pull_request.number) {
+          throw new Error('No number found in the pull_request.')
+        }
+
+        if (!github.context.payload.repository) {
+          throw new Error('No repository found in the payload.')
+        }
+
+        if (!github.context.payload.repository.name) {
+          throw new Error('No name found in the repository.')
+        }
+
+        if (
+          !github.context.payload.repository.owner ||
+          (!github.context.payload.repository.owner.login &&
+            !github.context.payload.repository.owner.name)
+        ) {
+          throw new Error('No owner found in the repository.')
+        }
+
         const commitMessages = await getCommitMessagesFromPullRequest(
-          pullRequestOptions.accessToken
+          pullRequestOptions.accessToken,
+          github.context.payload.repository.owner.name ??
+            github.context.payload.repository.owner.login,
+          github.context.payload.repository.name,
+          github.context.payload.pull_request.number
         )
-        for (const i in commitMessages) {
-          if (commitMessages[i]) {
-            messages.push(commitMessages[i])
+
+        for (const message of commitMessages) {
+          if (message) {
+            messages.push(message)
           }
         }
       }
@@ -133,17 +187,24 @@ async function getMessages(
       break
     }
     case 'push': {
+      if (!github.context.payload) {
+        throw new Error('No payload found in the context.')
+      }
+
       if (
-        github.context.payload &&
-        github.context.payload.commits &&
-        github.context.payload.commits.length
+        !github.context.payload.commits ||
+        !github.context.payload.commits.length
       ) {
-        for (const i in github.context.payload.commits) {
-          if (github.context.payload.commits[i].message) {
-            messages.push(github.context.payload.commits[i].message)
-          }
+        core.debug(' - skipping commits')
+        break
+      }
+
+      for (const i in github.context.payload.commits) {
+        if (github.context.payload.commits[i].message) {
+          messages.push(github.context.payload.commits[i].message)
         }
       }
+
       break
     }
     default: {
@@ -155,45 +216,72 @@ async function getMessages(
 }
 
 async function getCommitMessagesFromPullRequest(
-  accessToken: string
-): Promise<any> {
-  const pullRequest = github.context.payload.pull_request
-  const repo = github.context.payload.repository
-  if (!repo || !pullRequest) return null
+  accessToken: string,
+  repositoryOwner: string,
+  repositoryName: string,
+  pullRequestNumber: number
+): Promise<string[]> {
+  core.debug('Get messages from pull request...')
+  core.debug(` - accessToken: ${accessToken}`)
+  core.debug(` - repositoryOwner: ${repositoryOwner}`)
+  core.debug(` - repositoryName: ${repositoryName}`)
+  core.debug(` - pullRequestNumber: ${pullRequestNumber}`)
 
-  const body = {
-    query: `
-    query{
-      repository(owner: "${repo.owner.name}", name:"${repo.name}") {
-        pullRequest(number: ${pullRequest.number}) {
-          commits (last: 100) {
-            edges {
-              node {
-                commit {
-                  message
-                }
+  const query = `
+  query commitMessages(
+    $repositoryOwner: String!
+    $repositoryName: String!
+    $pullRequestNumber: Int!
+    $numberOfCommits: Int = 100
+  ) {
+    repository(owner: $repositoryOwner, name: $repositoryName) {
+      pullRequest(number: $pullRequestNumber) {
+        commits(last: $numberOfCommits) {
+          edges {
+            node {
+              commit {
+                message
               }
             }
           }
         }
       }
     }
-    `
+  }
+`
+  const variables = {
+    repositoryOwner: repositoryOwner,
+    repositoryName: repositoryName,
+    pullRequestNumber: pullRequestNumber,
+    headers: {
+      authorization: `token ${accessToken}`
+    }
   }
 
-  const response = await fetch('https://api.github.com/graphql', {
-    method: 'post',
-    body: JSON.stringify(body),
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: 'bearer ' + accessToken
+  core.debug(` - query: ${query}`)
+  core.debug(` - variables: ${JSON.stringify(variables, null, 2)}`)
+
+  const {repository} = await graphql(query, variables)
+
+  core.debug(` - response: ${JSON.stringify(repository, null, 2)}`)
+
+  let messages: string[] = []
+
+  interface EdgeItem {
+    node: {
+      commit: {
+        message: string
+      }
     }
-  })
-  const json = await response.json()
-  const messages = json.data.repository.pullRequest.commits.edges.map(function(
-    commit: any
-  ) {
-    return commit.node.commit.message
-  })
+  }
+
+  if (repository.pullRequest) {
+    messages = repository.pullRequest.commits.edges.map(function(
+      edge: EdgeItem
+    ): string {
+      return edge.node.commit.message
+    })
+  }
+
   return messages
 }
