13e9626e7d33f266a006c680886959be564a9833
amar.khan <amarkotasky@gmail.com>
2023-04-17 09:44:56 +0200

    OPS-16616

--- dist/index.js
+++ dist/index.js
@@ -379,8 +379,8 @@ function getCommitMessagesFromPullRequest(accessToken, repositoryOwner, reposito
         core.debug(` - response: ${JSON.stringify(repository, null, 2)}`);
         let messages = [];
         var edgedata = repository.pullRequest.commits.edges;
-        var excludUsersStr = Array.from(excludUsersList).toString();
-        core.info(`testx ${excludUsersStr}`);
+        var excludUsersStr = excludUsersList.toString();
+        core.info(`testxxxss ${excludUsersStr}`);
         for (let i = 0; i < Object.keys(edgedata).length; i++) {
             if (excludUsersStr.includes(edgedata[i].node.commit.author.name)) {
                 core.info(`test314`);
--- src/input-helper.ts
+++ src/input-helper.ts
@@ -306,8 +306,8 @@ async function getCommitMessagesFromPullRequest(
 
   let messages: string[] = []
   var edgedata = repository.pullRequest.commits.edges
-  var excludUsersStr = Array.from(excludUsersList).toString()
-  core.info(`testx ${excludUsersStr}`)
+  var excludUsersStr = excludUsersList.toString()
+  core.info(`testxxxss ${excludUsersStr}`)
 
   for (let i = 0; i < Object.keys(edgedata).length; i++) {
     if (excludUsersStr.includes(edgedata[i].node.commit.author.name)) {
